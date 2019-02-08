3fee2c712f18d53c9ce95225674ccdeaf64d3ddd
zhangtravis <popcorn20020806@gmail.com>
2019-02-08 14:42:00 -0700

    Add files via upload

---
+++ cluster_startup_sh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo gcsfuse --implicit-dirs --only-dir=lead_scoring infusionsoft-looker-poc lead_scoring
---
+++ create_mount_directories_sh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo mkdir lead_scoring && sudo gcsfuse lead_scoring lead_scoring
+sudo apt-get install tmux -y
